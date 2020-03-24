2863c2e4d8943798a771568b5290aee341b81d2d
Darshan jodhawat <57899990+darshanjain033@users.noreply.github.com>
2020-03-25 01:01:52 +0530

    second commit

---
+++ Basic/range.py
@@ -0,0 +1,4 @@
+# Range() is used to generate a sequence of numbers.. 
+a=range(3,31,3)   #range(<startvalue>,<endvalue>,<stepsize>)
+for x in a:   # It is also used to print only integer number TABLE..
+    print(x)
