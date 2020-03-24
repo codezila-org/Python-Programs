dab25d881676352f83f19f467737a10489e69d85
Brajpal Panwar <57897948+Brajpalsingh@users.noreply.github.com>
2020-03-24 23:17:54 +0530

    Count Number.py

---
+++ Advance/Count Number.py
@@ -0,0 +1,11 @@
+# * Written By Codezila.org at 22-03-2020 02:28AM (IST)
+# Python Program to Count Number of Digits in a Number using While loop
+
+Number = int(input("Please Enter any Number: "))
+Count = 0
+while(Number > 0):
+    Number = Number // 10
+    Count = Count + 1
+
+print("\n Number of Digits in a Given Number = %d" %Count)
+print("\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>")
