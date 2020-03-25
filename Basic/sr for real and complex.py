52df684d5266c3b0cfc1871f62c53078d498956c
Brajpal Panwar <official.brajpal@gmail.com>
2020-03-25 13:42:07 +0530

    Update sr for real and complex.py
    CODEZILA | codePy

--- Basic/sr for real and complex.py
+++ Basic/sr for real and complex.py
@@ -1,3 +1,4 @@
+# * Written By Codezila.org at 20-03-2020 1:26AM (IST)
 # Find square root of real or complex numbers
 # Importing the complex math module
 import cmath
@@ -9,3 +10,4 @@ num = 1+2j
 
 num_sqrt = cmath.sqrt(num)
 print('The square root of {0} is {1:0.3f}+{2:0.3f}j'.format(num ,num_sqrt.real,num_sqrt.imag))
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
