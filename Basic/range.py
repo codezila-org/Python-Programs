69c2a6fdd0f3bea91e355bd3113c5079fa93c016
Brajpal Panwar <official.brajpal@gmail.com>
2020-03-25 13:38:38 +0530

    Update range.py
    CODEZILA | codePy

--- Basic/range.py
+++ Basic/range.py
@@ -1,4 +1,6 @@
+# * Written By Codezila.org at 18-03-2020 10:15PM (IST)
 # Range() is used to generate a sequence of numbers.. 
 a=range(3,31,3)   #range(<startvalue>,<endvalue>,<stepsize>)
 for x in a:   # It is also used to print only integer number TABLE..
     print(x)
+    print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
