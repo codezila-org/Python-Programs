c66b044aa58c7cc7b717440ac4d34706d2711b81
Brajpal Panwar <official.brajpal@gmail.com>
2020-03-25 13:49:59 +0530

    Update pro.py
    CODEZILA | codePy

--- Basic/addition/pro.py
+++ Basic/addition/pro.py
@@ -1,3 +1,4 @@
+# * Written By Codezila.org at 10-03-2020 11:17PM (IST)
 #addition of two numbers
 #input function is use for taking input from user
 a=input("Enter the first  Number: ")
@@ -6,3 +7,4 @@ b=input("Enter the second Number: ")
 c=int(a) + int(b)
 # Display the sum
 print("Sun of",a,"and",b,"is Equal To",c)
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
