4518a384fa327727679179c3b686e11203ec9c2c
Brajpal Panwar <official.brajpal@gmail.com>
2020-03-25 13:51:22 +0530

    Update additon.py
    CODEZILA | codePy

--- Basic/addition/additon.py
+++ Basic/addition/additon.py
@@ -1,3 +1,4 @@
+# * Written By Codezila.org at 12-03-2020 11:05PM (IST)
 # This program adds two numbers
 
 num1 = 1.5
@@ -8,3 +9,4 @@ sum = float(num1) + float(num2)
 
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
