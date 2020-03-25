f7b420eb572b2469005fd834f83538e6189d132e
Brajpal Panwar <official.brajpal@gmail.com>
2020-03-25 13:46:07 +0530

    Update swap without tem.py
    CODEZILA | codePy

--- Basic/swap without tem.py
+++ Basic/swap without tem.py
@@ -1,3 +1,4 @@
+# * Written By Codezila.org at 21-03-2020 2:29AM (IST)
 #swaping without using temporary variable
 x = 5
 y = 10
@@ -5,3 +6,4 @@ y = 10
 x, y = y, x
 print("x =", x)
 print("y =", y)
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
