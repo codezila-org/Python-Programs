e748462dd17cdd3e0632bf06ee75916f6bb12c3c
Brajpal Panwar <official.brajpal@gmail.com>
2020-03-25 13:36:42 +0530

    Update random no .py
    CODEZILA | codePy

--- Basic/random no .py
+++ Basic/random no .py
@@ -1,6 +1,8 @@
+# * Written By Codezila.org at 18-03-2020 9:13PM (IST)
 # Program to generate a random number between 0 and 9
 
 # importing the random module
 import random
 
 print(random.randint(0,9))
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
