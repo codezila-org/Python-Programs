a5405ec74e0e1064d6d5927fb0ecdda98e1f6a10
Brajpal Panwar <official.brajpal@gmail.com>
2020-03-25 13:33:33 +0530

    Update quadratic.py
    CODEZILA | codePy

--- Basic/quadratic.py
+++ Basic/quadratic.py
@@ -1,3 +1,4 @@
+# * Written By Codezila.org at 17-03-2020 10:08AM (IST)
 # import complex math module  
 import cmath  
 a = float(input('Enter a: '))  
@@ -11,3 +12,4 @@ d = (b**2) - (4*a*c)
 sol1 = (-b-cmath.sqrt(d))/(2*a)  
 sol2 = (-b+cmath.sqrt(d))/(2*a)  
 print('The solution are {0} and {1}'.format(sol1,sol2))
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
