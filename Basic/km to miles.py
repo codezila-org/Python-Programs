a205f05582799758114eeebb91b697f604e4610f
Brajpal Panwar <official.brajpal@gmail.com>
2020-03-25 13:29:37 +0530

    Update km to miles.py
    CODEZILA | codePy

--- Basic/km to miles.py
+++ Basic/km to miles.py
@@ -1,4 +1,6 @@
+# * Written By Codezila.org at 16-03-2020 11:28PM (IST)
 # Taking kilometers input from the user
+
 kilometers = float(input("Enter value in kilometers: "))
 
 # conversion factor
@@ -7,3 +9,4 @@ conv_fac = 0.621371
 # calculate miles
 miles = kilometers * conv_fac
 print('%0.2f kilometers is equal to %0.2f miles' %(kilometers,miles))
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
