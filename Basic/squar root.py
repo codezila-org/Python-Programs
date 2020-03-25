29bf89a48c42894a5ce2c8c92af0d1bf0491fd56
Brajpal Panwar <official.brajpal@gmail.com>
2020-03-25 13:40:22 +0530

    Update squar root.py
    CODEZILA | codePy

--- Basic/squar root.py
+++ Basic/squar root.py
@@ -1,3 +1,4 @@
+# * Written By Codezila.org at 19-03-2020 11:00PM (IST)
 # Python Program to calculate the square root
 
 # Note: change this value for a different result
@@ -8,3 +9,4 @@ num = 8
 
 num_sqrt = num ** 0.5
 print('The square root of %0.3f is %0.3f'%(num ,num_sqrt))
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
