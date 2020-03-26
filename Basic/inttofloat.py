d5acc5c4ee74c4be2f80de5c1c5c9a495767ae22
Darshan jodhawat <57899990+darshanjain033@users.noreply.github.com>
2020-03-26 13:31:48 +0530

    first commit

---
+++ Basic/inttofloat.py
@@ -0,0 +1,5 @@
+# * Written By Codezila.org at 26-03-2020 01:30PM (IST)
+#Program for print integer to float number... 
+int=65
+print(float(int))
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
