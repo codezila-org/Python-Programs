15981729576b7be10ff9241dfa49b6bf96b04c12
Darshan jodhawat <57899990+darshanjain033@users.noreply.github.com>
2020-03-26 13:27:18 +0530

    update ord( )

--- Basic/ASCII.py
+++ Basic/ASCII.py
@@ -1,5 +1,5 @@
 # * Written By Codezila.org at 26-03-2020 12:50PM (IST)
 #Program for print ASCII value of string...
 a="C"  
-print("The ASCII value of C is :",ord(a))
+print("The ASCII value of C is :",ord(a)) #ord() is used to convert character to integer..
 print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
