085898db2b53bc35813a5f7c3371af47c5a1c31b
Darshan jodhawat <57899990+darshanjain033@users.noreply.github.com>
2020-03-26 13:11:24 +0530

    first commit

---
+++ Basic/deciconversion.py
@@ -0,0 +1,9 @@
+# * Written By Codezila.org at 26-03-2020 01:05PM (IST)
+#Program for print decimal into binary ,octal and hexadecimal numbers... 
+decimal=255
+
+print("The decimal value of", decimal, "is :")
+print(bin(decimal),"in binary")      #binary conversion.
+print(oct(decimal),"in octal")       #octal conversion.
+print(hex(decimal),"in hexadecimal") #hexadecimal conversion.
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
