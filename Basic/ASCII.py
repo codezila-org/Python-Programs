8cac0c781db2ec1ed947ad70ff7527e81348811a
Darshan jodhawat <57899990+darshanjain033@users.noreply.github.com>
2020-03-26 12:55:46 +0530

    second commit

--- Basic/ASCII.py
+++ Basic/ASCII.py
@@ -1,4 +1,4 @@
-# * Written By Codezila.org at 26-03-2020 12:30PM (IST)
+# * Written By Codezila.org at 26-03-2020 12:50PM (IST)
 #Program for print ASCII value of string...
 a="C"  
 print("The ASCII value of C is :",ord(a))
