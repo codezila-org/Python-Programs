1be0f308fef356dcde8db22cbc23794faa16ea35
Darshan jodhawat <57899990+darshanjain033@users.noreply.github.com>
2020-03-26 21:01:43 +0530

    def add()

---
+++ Basic/def add().py
@@ -0,0 +1,11 @@
+# * Written By Codezila.org at 26-03-2020 09:00PM (IST)
+#Program for print integer to float number... 
+x=5
+def add():
+  x=3
+  x=3+5
+  print(x)
+add()
+print(x)
+
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
