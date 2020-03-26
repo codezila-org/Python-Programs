6536f64a13babfd960097f80d70945fdb364e2e4
Darshan jodhawat <57899990+darshanjain033@users.noreply.github.com>
2020-03-26 12:54:38 +0530

    first commit

---
+++ Basic/ASCII.py
@@ -0,0 +1,5 @@
+# * Written By Codezila.org at 26-03-2020 12:30PM (IST)
+#Program for print ASCII value of string...
+a="C"  
+print("The ASCII value of C is :",ord(a))
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
