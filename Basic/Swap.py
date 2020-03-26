811c6eb4def284923e8c9fda3411b3065e2c4158
Darshan jodhawat <57899990+darshanjain033@users.noreply.github.com>
2020-03-26 21:08:43 +0530

    Swapping numbers

---
+++ Basic/Swap.py
@@ -0,0 +1,9 @@
+# * Written By Codezila.org at 26-03-2020 09:00PM (IST)
+#Program for swap number...
+x = 5
+y = 6
+z = 7
+x,y,z = x,z,y
+print(x+y)
+
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
