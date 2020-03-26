c9dd990510fb81e7d3ba177a660ebacee7025cf1
Darshan jodhawat <57899990+darshanjain033@users.noreply.github.com>
2020-03-26 13:19:59 +0530

    first commit

---
+++ Basic/while2.py
@@ -0,0 +1,10 @@
+# * Written By Codezila.org at 26-03-2020 01:20PM (IST)
+#Program for print decimal into binary ,octal and hexadecimal numbers... 
+num=5
+sum=0
+i=0
+while(i<num):
+    sum+=i
+    i+=1
+    print(i)
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
