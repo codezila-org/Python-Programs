c57e315bf42b04bf7d67394071a368513ee710cf
Darshan jodhawat <57899990+darshanjain033@users.noreply.github.com>
2020-03-26 12:23:41 +0530

    first commit

---
+++ Basic/if-else.py
@@ -0,0 +1,9 @@
+# * Written By Codezila.org at 26-03-2020 12:20PM (IST)
+#program for if-else statement..
+a=10
+b=20
+if(a!=b): #condition with expression 
+    print("welcome to the codezila :")
+else:
+    print("lets try something new :")
+    print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
