a38ecfd83d70c26a61b10b6734dcb3e165e4d66c
Darshan jodhawat <57899990+darshanjain033@users.noreply.github.com>
2020-03-26 12:10:53 +0530

    third commit

--- Basic/While.py
+++ Basic/While.py
@@ -3,3 +3,4 @@ i=1  #initialization
 while(i<8): #condition with expression
     print(i)
     i+=1
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
