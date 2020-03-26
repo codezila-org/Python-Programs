47a9cebe286a80f25840f98cb6cc174ca308ff5f
Darshan jodhawat <57899990+darshanjain033@users.noreply.github.com>
2020-03-26 12:40:50 +0530

    first commit

---
+++ Basic/Swapcase.py
@@ -0,0 +1,7 @@
+# * Written By Codezila.org at 26-03-2020 12:40PM (IST)
+#Program for SWAPCASE() method...
+A="codezila"  #String initialize.
+B="WELCOME"
+print(A.swapcase())  #it converts string lowercase to uppercase and vice-versa.
+print(B.swapcase())
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
