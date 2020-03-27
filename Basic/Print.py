42e4beda6d8bc5b738df474c821bd731001f21da
Darshan jodhawat <57899990+darshanjain033@users.noreply.github.com>
2020-03-27 18:16:49 +0530

    Hello World

---
+++ Basic/Print.py
@@ -0,0 +1,5 @@
+# * Written By Codezila.org at 27-03-2020 06:10PM (IST)
+#Program for print Hello World...
+print("Hello World :)")
+
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
