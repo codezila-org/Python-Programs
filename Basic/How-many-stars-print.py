84231a77d7dcbe85124f36bc6ef0f8eb2193403c
Aman Singour <amansingour15@gmail.com>
2021-05-19 23:16:23 +0530

    Update and rename How-many-stars-print to How-many-stars-print.py
    Extension added. Indentation fixed.

--- Basic/How-many-stars-print.py
+++ Basic/How-many-stars-print.py
@@ -8,4 +8,4 @@ while(i>0):   #condition
         break
     else:
         print("*")
-        print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
+print('\n\tCode By codezila.org :)\n\tJoin Us On : <github.com/codezila-org>\n\tFor More Mail Us : <contact@codezila.org>')
